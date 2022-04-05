ccf8fdd0bd55c91da4a4fcd5d14c231777ecccdd
aishahsofea <aishah.s.hanifa@gmail.com>
2022-04-05 21:16:34 +0800

    pages & file-based routing

---
+++ pages/clients/[id]/index.js
@@ -0,0 +1,20 @@
+import { useRouter } from "next/router";
+
+function ClientProjectsPage() {
+  const router = useRouter();
+
+  function loadProjectHandler() {
+    router.push({
+      pathname: "/clients/[id]/[clientprojectid]",
+      query: { id: "max", clientprojectid: "projecta" },
+    });
+  }
+  return (
+    <div>
+      <h1>The Projects of a Given Client</h1>
+      <button onClick={loadProjectHandler}>Load Project</button>
+    </div>
+  );
+}
+
+export default ClientProjectsPage;
