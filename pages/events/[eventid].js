07911de83c79491a1c0de40b33ecd6870399f764
aishahsofea <aishah.s.hanifa@gmail.com>
2022-04-14 21:55:40 +0800

    adding api routes

--- pages/events/[eventid].js
+++ pages/events/[eventid].js
@@ -2,6 +2,7 @@ import Head from "next/head";
 import EventContent from "../../components/event-detail/event-content";
 import EventLogistics from "../../components/event-detail/event-logistics";
 import EventSummary from "../../components/event-detail/event-summary";
+import Comments from "../../components/input/comments";
 import { getEventById, getFeaturedEvents } from "../../helpers/api-util";
 
 function EventDetailPage(props) {
@@ -32,6 +33,7 @@ function EventDetailPage(props) {
       <EventContent>
         <p>{event.description}</p>
       </EventContent>
+      <Comments eventId={event.id} />
     </>
   );
 }
