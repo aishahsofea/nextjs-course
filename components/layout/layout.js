b77aa4f230bb2d0152e72c2162c33b40ba2ba0d7
aishahsofea <aishah.s.hanifa@gmail.com>
2022-04-17 00:20:38 +0800

    implement react context

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,10 +1,23 @@
+import { useContext } from "react";
+import Notification from "../../components/ui/notification";
+import NotificationContext from "../../store/notification-context";
 import MainHeader from "./main-header";
 
 function Layout(props) {
+  const notificationCtx = useContext(NotificationContext);
+  const activeNotification = notificationCtx.notification;
+
   return (
     <>
       <MainHeader />
       <main>{props.children}</main>
+      {activeNotification && (
+        <Notification
+          title={activeNotification.title}
+          message={activeNotification.message}
+          status={activeNotification.status}
+        />
+      )}
     </>
   );
 }
